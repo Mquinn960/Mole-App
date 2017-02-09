a7d7ae3f3a8794bd13111f42b5e3fa5403c6ba82
Nathrakh960 <matt@mquinn.co.uk>
2017-02-09 21:14:21 +0000

    added static assets to game play screen

--- app/src/main/java/mquinn/whackamole/GameActivity.java
+++ app/src/main/java/mquinn/whackamole/GameActivity.java
@@ -2,8 +2,26 @@ package mquinn.whackamole;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.os.CountDownTimer;
 
 public class GameActivity extends AppCompatActivity {
+
+
+
+    new CountDownTimer(30000, 1000) {
+
+        public void onTick(long millisUntilFinished) {
+            mTextField.setText("seconds remaining: " + millisUntilFinished / 1000);
+        }
+
+    public void onFinish() {
+        mTextField.setText("done!");
+    }
+}.start();
+
+
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
